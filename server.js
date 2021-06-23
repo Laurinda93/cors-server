714f59a980860dfd972adac675039b9c77a5f811
Laurinda93 <wjy1993@gmail.com>
2021-06-23 00:27:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
     //originWhitelist: ['http://localhost:3000', 'https://pppppppppppproxy.herokuapp.com'],
-    requireHeader: ['origin', 'x-requested-with'],
+    //requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
